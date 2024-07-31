5bc7c10ba1a558650043e2d307ca3147c5ab9bfd
yaaros <3039994815@qq.com>
2024-07-31 21:30:16 +0800

    222

---
+++ src/main/java/com/yaaros/hyrah/loot/AddItemModifier.java
@@ -0,0 +1,50 @@
+package com.yaaros.hyrah.loot;
+
+import com.google.common.base.Suppliers;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
+import net.minecraftforge.common.loot.IGlobalLootModifier;
+import net.minecraftforge.common.loot.LootModifier;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Supplier;
+
+public class AddItemModifier extends LootModifier {
+    //编解码器
+    public static final Supplier<Codec<AddItemModifier>> CODEC = Suppliers.memoize(()
+            -> RecordCodecBuilder.create(inst -> codecStart(inst).and(ForgeRegistries.ITEMS.getCodec()
+            .fieldOf("item").forGetter(m -> m.item)).apply(inst, AddItemModifier::new)));
+    public final Item item;
+    /**
+     * Constructs a LootModifier.
+     *
+     * @param conditionsIn the ILootConditions that need to be matched before the loot is modified.
+     */
+    protected AddItemModifier(LootItemCondition[] conditionsIn,Item item) {
+        super(conditionsIn);
+        this.item = item;
+    }
+
+    @Override
+    protected @NotNull ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> generatedLoot, LootContext context) {
+        //This basically allows us to add item conditions or loot item conditions to Global Modifiers
+        for (LootItemCondition condition : this.conditions) {
+            if(!condition.test(context)){
+                return generatedLoot;
+            }
+        }
+        generatedLoot.add(new ItemStack(this.item));
+        return generatedLoot;
+    }
+
+    @Override
+    public Codec<? extends IGlobalLootModifier> codec() {
+        return null;
+    }
+}
